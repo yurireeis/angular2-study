5987c30e4625182158cfd3cdc45d106afa655e59
Yuri Reis <yuri.reis@msn.com>
2018-06-01 12:07:04 -0300

    Directives chapter

---
+++ diretivas/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent,
+    DiretivaNgclassComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = ['Um curso'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
